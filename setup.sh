dc84808487bd19dab5e3016cdbb8ba18d406eacd
unixonbooard <unixonboard@gmail.com>
2021-02-20 19:15:38 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 mkdir ansible
 cd ansible
 sudo yum install git -y
-sleep 20
+sleep 5
 git init
 git remote add upstream https://github.com/unixonboard/ansible-test.git
 git clone https://github.com/unixonboard/ansible-test.git
 sudo yum install vim -y
-sleep 20
+sleep 5
 sudo yum install python3 -y
-sleep 20
+sleep 5
 sudo alternatives --set python /usr/bin/python3
 sudo yum -y install python3-pip
-sleep 20
+sleep 5
 pip3 install ansible --user
-sleep 20
-
+sleep 5
+echo "export ANSIBLE_HOSTKEY_CHECKING=False" >> .bash_profile
